ac851f6439645457a275578feba6f57745989f68
Sandeep Singh <serperior2@gmail.com>
2022-02-19 16:26:05 -0500

    create scripts to parse Lean data

---
+++ storage/app/public/parse-subs-specials.js
@@ -0,0 +1,112 @@
+const fs = require("fs");
+
+let subSpecialResult = {
+    'subs': {},
+    'specials': {}
+};
+
+
+console.log("Prepping data to write...");
+let subDone = false;
+let specialDone = false;
+
+
+// read sub data
+fs.readFile(__dirname + '/550/WeaponInfo_Sub.json', function(err, data) {
+    if (err) {
+        return console.error(err);
+    }
+
+    const subData = JSON.parse(data);
+    subData.forEach(sub => {
+        if (sub.Id < 100) {
+            const subName = sub.Name;
+            let subInternalName = subName;
+
+            if (subName.includes("Bomb_")) {
+                subInternalName = subName.replace("Bomb_", "Bomb")
+            }
+            if (subName == "TimerTrap") {
+                subInternalName = "Trap";
+            }
+            if (subName.includes("Poison") || subName.includes("Point")) {
+                subInternalName = "Bomb" + subName;
+            }
+            if (subName == "Flag") {
+                subInternalName = "JumpBeacon";
+            }
+
+            subSpecialResult.subs[subInternalName] = sub;
+
+            fs.readFile(__dirname + '/550/WeaponBullet/' + subInternalName + '.json', function(err, bulletData) {
+                if (err) {
+                    return console.error(err);
+                }
+                
+                bulletData = JSON.parse(bulletData);
+                subSpecialResult.subs[subInternalName]['param'] = bulletData['param'];
+            });
+        }
+
+        if (sub.Name === 'VictoryClam') {
+            subDone = true;
+        }
+    });
+});
+
+
+// read special data
+fs.readFile(__dirname + '/550/WeaponInfo_Special.json', function(err, data) {
+    if (err) {
+        return console.error(err);
+    }
+
+    const specialData = JSON.parse(data);
+    specialData.forEach(special => {
+        if (special.Id != 15 && 
+            special.Id != 16 && 
+            special.Id != 13 && 
+            special.Id <= 18 
+        ) {
+            let specialInternalName = special.Name;
+
+            if (special.Name.includes("Launcher")) {
+                specialInternalName = "Bomb" + special.Name.replace("Launcher", "") + "Launcher";
+            }
+
+            subSpecialResult.specials[specialInternalName] = special;
+
+            fs.readFile(__dirname + '/550/WeaponBullet/' + specialInternalName + '.json', function(err, bulletData) {
+                if (err) {
+                    return console.error(err);
+                }
+                
+                bulletData = JSON.parse(bulletData);
+                subSpecialResult.specials[specialInternalName]['param'] = bulletData['param'];
+            });
+        }
+
+        if (special.Name === 'BigLaser') {
+            specialDone = true;
+        }
+    });
+});
+
+
+// async reading above. Check every second if reading data is "done"
+setInterval(() => {
+    if (subDone && specialDone) {
+        const preppedData = JSON.stringify(subSpecialResult);
+        
+        fs.writeFileSync(__dirname + '/550/parsed-subs-specials-results.json', preppedData, function(err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("Data written successfully!");
+            // return 1;
+        });
+        subDone = false;
+        specialDone = false;
+        // return 1;
+    } 
+}, 1000); // ctrl + C to exit script
---
+++ storage/app/public/parse-weapons.js
@@ -0,0 +1,112 @@
+const fs = require("fs");
+
+let weaponResult = {};
+
+
+
+console.log("Prepping data to write...");
+let done = false;
+
+fs.readFile(__dirname + '/550/WeaponInfo_Main.json', function(err, data) {
+    if (err) {
+        return console.error(err);
+    }
+
+    const weaponData = JSON.parse(data);
+    weaponData.forEach(weapon => {
+        // get weapon name
+        const weaponNameSplit = weapon["Name"].split("_");
+        let weaponName = weaponNameSplit[0] + weaponNameSplit[1];
+
+        if (weaponName.includes("Blaster")) {
+            weaponName = weaponName.substring(7);
+        }
+
+
+        // bullet stat
+        fs.readFile(__dirname + '/550/WeaponBullet/' + weaponName + '.json', function(err, wbData) {
+            if (err) {
+                return console.error(err);
+            }
+
+
+            if (weaponName in weaponResult) {
+                // weapon group exists, just add weapon info
+                weaponResult[weaponName][weapon.Name] = weapon;
+            }
+            else {
+                // weapon group does not exist, add weapon group stats and weapon info
+                wbData = JSON.parse(wbData);
+                weaponResult[weaponName] = wbData; // params {}
+
+                weaponResult[weaponName][weapon.Name] = weapon;
+
+
+                // add extra stats if need be
+                if (weaponName.includes("Spinner") || weaponName.includes("Twins")) {
+                    fs.readFile(__dirname + '/550/WeaponBullet/' + weaponName + '_2.json', function(err, wbData2) {
+                        if (err) {
+                            return console.error(err);
+                        }
+
+                        wbData2 = JSON.parse(wbData2);
+                        weaponResult[weaponName][weaponName + '_2.json'] = wbData2['param'];
+                    });
+                }
+                else if (weaponName.includes("Blaster")) {
+                    fs.readFile(__dirname + '/550/WeaponBullet/' + weaponName + '_Burst.json', function(err, wbData2) {
+                        if (err) {
+                            return console.error(err);
+                        }
+
+                        wbData2 = JSON.parse(wbData2);
+                        weaponResult[weaponName][weaponName + '_burst'] = wbData2['param'];
+                    });
+                }
+                else if (weaponName.includes("Roller")) {
+                    fs.readFile(__dirname + '/550/WeaponBullet/' + weaponName + '_Stand.json', function(err, standData) {
+                        if (err) {
+                            return console.error(err);
+                        }
+
+                        fs.readFile(__dirname + '/550/WeaponBullet/' + weaponName + '_Jump.json', function(err, jumpData) {
+                            if (err) {
+                                return console.error(err);
+                            }
+
+                            standData = JSON.parse(standData);
+                            jumpData = JSON.parse(jumpData);
+
+                            weaponResult[weaponName][weaponName + '_stand'] = standData['param'];
+                            weaponResult[weaponName][weaponName + '_jump'] = jumpData['param'];
+                        });
+                    });
+                }
+            }
+            
+            // if last entry
+            if (weapon.Name === 'Umbrella_Compact_02') {
+                done = true;
+            }
+        }); 
+    });
+});
+
+
+
+// async reading above. Check every second if reading data is "done"
+setInterval(() => {
+    if (done) {
+        const preppedData = JSON.stringify(weaponResult);
+        
+        fs.writeFileSync(__dirname + '/550/parsed-weapons-results.json', preppedData, function(err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("Data written successfully!");
+            // return 1;
+        });
+        done = false;
+        // return 1;
+    } 
+}, 1000); // ctrl + C to exit script
