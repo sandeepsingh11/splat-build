1a1f4e2bf1198e16ac4a138c651ad2c003537f46
Sandeep Singh <serperior2@gmail.com>
2022-05-16 19:29:18 -0400

    add search-select logic

--- resources/frontend/angular/src/app/pages/gear/gear-form/gear-form.component.ts
+++ resources/frontend/angular/src/app/pages/gear/gear-form/gear-form.component.ts
@@ -24,6 +24,7 @@ export class GearFormComponent implements OnInit {
     // get gears
     this.leanDataService.getBaseGears().subscribe((data) => {
       this.baseGears = data;
+      this.baseGearFilteredList = data;
     });
   }
 
@@ -33,10 +34,33 @@ export class GearFormComponent implements OnInit {
   mainSkills: SkillResponse[] = [];
   otherSkills: SkillResponse[] = [];
   baseGears!: BaseGearResponse[];
+  baseGearFilteredList: BaseGearResponse[] = [];
   title: string = 'Create Gear';
   selectedGear: string = 'Hed_FST000';
   skillMain: string = 'Unknown';
   skill1: string = 'Unknown';
   skill2: string = 'Unknown';
   skill3: string = 'Unknown';
+  searchTerm: string = '';
+
+  // filter gear list by search term
+  search(e: Event): void {
+    this.searchTerm = (e.target as HTMLInputElement).value;
+
+    // if search term is empty, set default
+    if (this.searchTerm === '') {
+      this.baseGearFilteredList = this.baseGears;
+      this.selectedGear = this.baseGears[0].base_gear_name;
+      return;
+    }
+
+    // filter
+    this.baseGearFilteredList = this.baseGears.filter((gear: BaseGearResponse) => {
+      return (gear.display_name.toLocaleLowerCase().indexOf(this.searchTerm.toLocaleLowerCase()) > -1);
+    });
+
+    // set selected gear
+    if (this.baseGearFilteredList.length > 0) this.selectedGear = this.baseGearFilteredList[0].base_gear_name;
+    else this.selectedGear = this.baseGears[0].base_gear_name;
+  }
 }
